9bd1750ebfbc9b0b714b8a49723180d08641fdb7
Harris Shen <srh9572@gmail.com>
2023-05-04 16:37:15 -0400

    add login status check

--- ca02/routes/rap.js
+++ ca02/routes/rap.js
@@ -1,29 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const { openai } = require("../util");
+const { isLoggedIn, openai } = require("../util");
 
-router.get("/rap", (req, res) => {
-  res.render("rap/index");
+router.get("/rap",
+  isLoggedIn,
+  (req, res) => {
+    res.render("rap/index");
 });
 
-router.post("/rap", async (req, res) => {
-  const prompt = req.body.prompt;
-  try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(prompt),
-      temperature: 0.8,
-      max_tokens: 1024,
-    });
+router.post("/rap", 
+  isLoggedIn,
+  async (req, res) => {
+    const prompt = req.body.prompt;
+    try {
+      const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: generatePrompt(prompt),
+        temperature: 0.8,
+        max_tokens: 1024,
+      });
 
-    // pass prompt and answer parameters
-    res.render(
-      "rap/result", 
-      {prompt: prompt, answer: completion.data.choices[0].text}
-    )
-  } catch(error) {
-    console.log(error);
-  }
+      // pass prompt and answer parameters
+      res.render(
+        "rap/result", 
+        {prompt: prompt, answer: completion.data.choices[0].text}
+      )
+    } catch(error) {
+      console.log(error);
+    }
 });
 
 function generatePrompt(prompt) {
