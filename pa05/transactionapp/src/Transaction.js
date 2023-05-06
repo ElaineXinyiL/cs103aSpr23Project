ab8d9b94c31ab185a049fe3c701dab9a0d51ca16
Harris Shen <srh9572@gmail.com>
2023-05-06 15:54:58 -0400

    add summarize by date

--- pa05/transactionapp/src/Transaction.js
+++ pa05/transactionapp/src/Transaction.js
@@ -3,7 +3,7 @@ import "./styles.css";
 
 function getItemsFromLocalStorage() {
   // getting stored value
-  const saved = localStorage.getItem("items");
+  const saved = localStorage.getItem("transactionItems");
   const initialValue = JSON.parse(saved) || [];
   // relabel the keys from 0 to length-1
   for (let i = 0; i < initialValue.length; i++) {
@@ -19,6 +19,7 @@ export default function Transaction() {
   const [category, setCategory] = useState("");
   const [date, setDate] = useState("");
   const [description, setDescription] = useState("");
+  const [summarize, setSummarize] = useState(""); // summarization mode, default is "" (all items)
 
   /**
    * add an item to the transaction
@@ -44,7 +45,29 @@ export default function Transaction() {
     console.log(key);
     const newItems = items.filter((x) => x["itemId"] !== key);
     setItems(newItems);
-    setNumKeys(numKeys - 1);
+    // setNumKeys(numKeys - 1);
+  }
+
+  /**
+   * summarize the transaction by date
+   * written by Ruihao Shen
+   */
+  function summarizeByDate() {
+    console.log("summarize by date");
+    return items.reduce((acc, item) => { // copilot helped here, very smart use of reduce
+      const found = acc.find((x) => x.date === item.date);
+      if (found) {
+        found.amount += parseFloat(item.amount);
+        found.count += 1;
+      } else {
+        acc.push({
+          date: item.date,
+          amount: parseFloat(item.amount),
+          count: 1,
+        });
+      }
+      return acc;
+    }, []);
   }
 
   /**
@@ -53,93 +76,142 @@ export default function Transaction() {
    */
   useEffect(() => {
     // storing items if items changes value
-    localStorage.setItem("items", JSON.stringify(items));
+    localStorage.setItem("transactionItems", JSON.stringify(items));
   }, [items]);
+
+  /**
+   * content to show - all items or summarized items
+   * edited by Yixuan He, Ruihao Shen
+   */
+  let content;
+  switch (summarize) {
+    case "date":
+      const summarizeItems = summarizeByDate();
+      content = (
+        <>
+          <h2>Summarize by Date</h2>
+
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Amount</th>
+                <th>Item count</th>
+              </tr>
+            </thead>
+            <tbody>
+              {summarizeItems.map((item) => (
+                <tr key={item.date}>
+                  <td>{item.date}</td>
+                  <td>{item.amount}</td>
+                  <td>{item.count}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      );
+      break;
+    default:
+      content = (
+        <>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Item ID</th>
+                <th>Amount</th>
+                <th>Category</th>
+                <th>Date</th>
+                <th>Description</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((transaction) => (
+                <tr key={transaction.itemId}>
+                  <td>{transaction.itemId}</td>
+                  <td>{transaction.amount}</td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.date}</td>
+                  <td>{transaction.description}</td>
+                  <td>
+                    <button
+                      onClick={() => handleDeleteTransaction(transaction.itemId)}
+                      class="btn btn-primary"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          <h2> add new transaction </h2>
+          <form class="mb-3 g-3" onSubmit={handleAddTransaction}>
+            <div class="col-md-6">
+              <label class="form-label">
+                Amount:
+                <input
+                  type="number"
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                  class="form-control"
+                />
+              </label>
+            </div>
+            <div class="col-md-6">
+              <label class="form-label">
+                Category:
+                <input
+                  type="text"
+                  value={category}
+                  onChange={(e) => setCategory(e.target.value)}
+                  class="form-control"
+                />
+              </label>
+            </div>
+            <div class="col-md-6">
+              <label class="form-label">
+                Date:
+                <input
+                  type="date"
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                  class="form-control"
+                />
+              </label>
+            </div>
+            <div class="col-md-6">
+              <label class="form-label">
+                Description:
+                <input
+                  type="text"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  class="form-control"
+                />
+              </label>
+            </div>
+            <button type="submit" class="btn btn-primary">Add Transaction</button>
+          </form>
+        </>
+      );
+  }
 
   return (
     <div className="App container">
       <h1 className="bg-warning text-center p-2">Transaction</h1>
 
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Item ID</th>
-            <th>Amount</th>
-            <th>Category</th>
-            <th>Date</th>
-            <th>Description</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((transaction) => (
-            <tr key={transaction.itemId}>
-              <td>{transaction.itemId}</td>
-              <td>{transaction.amount}</td>
-              <td>{transaction.category}</td>
-              <td>{transaction.date}</td>
-              <td>{transaction.description}</td>
-              <td>
-                <button
-                  onClick={() => handleDeleteTransaction(transaction.itemId)}
-                  class="btn btn-primary"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="container">
+        <button type="button" class="btn btn-primary me-2" onClick={() => setSummarize("")}>Show all transactions</button>
+        <button type="button" class="btn btn-primary me-2" onClick={() => setSummarize("date")}>Summarize by date</button>
+        <button type="button" class="btn btn-primary me-2" onClick={() => setSummarize("month")}>Summarize by month</button>
+        <button type="button" class="btn btn-primary me-2" onClick={() => setSummarize("year")}>Summarize by year</button>
+        <button type="button" class="btn btn-primary me-2" onClick={() => setSummarize("category")}>Summarize by category</button>
+      </div>
 
-      <h2> add new transaction </h2>
-      <form class="mb-3 g-3" onSubmit={handleAddTransaction}>
-        <div class="col-md-6">
-          <label class="form-label">
-            Amount:
-            <input
-              type="number"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-              class="form-control"
-            />
-          </label>
-        </div>
-        <div class="col-md-6">
-          <label class="form-label">
-            Category:
-            <input
-              type="text"
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-              class="form-control"
-            />
-          </label>
-        </div>
-        <div class="col-md-6">
-          <label class="form-label">
-            Date:
-            <input
-              type="date"
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
-              class="form-control"
-            />
-          </label>
-        </div>
-        <div class="col-md-6">
-          <label class="form-label">
-            Description:
-            <input
-              type="text"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              class="form-control"
-            />
-          </label>
-        </div>
-        <button type="submit" class="btn btn-primary">Add Transaction</button>
-      </form>
+      {content}
     </div>
   );
 }
