4913a7f3f7ab9cfe5aced70810f24b5d22491345
ElaineXinyiL <liuxinyi_sufe@163.com>
2023-05-05 20:12:12 -0400

    add field in prompt model

--- ca02/models/Prompt.js
+++ ca02/models/Prompt.js
@@ -6,7 +6,8 @@ var promptSchema = Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   requests: String,
   answer: String,
-  createdAt: {type: Date, default: Date.now},
+  createdAt: { type: Date, default: Date.now },
+  demo: String,
 });
 
 module.exports = mongoose.model("Prompt", promptSchema);
