3a4be14e77991a02bfc0f5ec5989c9d75a18bf4e
Lyhq1996 <lihongqian_shufe@163.com>
2023-05-07 21:27:29 -0400

    add prompt model into joke page

--- ca02/routes/joke.js
+++ ca02/routes/joke.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router();
-// const User = require('../models/User')
-const { openai } = require("../util");
+const PromptItem = require('../models/Prompt');
+const { isLoggedIn, openai } = require("../util");
 
-router.get("/joke", (req, res) => {
-  res.render("joke");
+router.get("/joke", 
+  isLoggedIn,
+  async (req, res, next) => {
+    const items = await PromptItem.find({userId:req.user._id, demo:"joke"})
+                                  .sort({createdAt:1});
+    res.render("joke", {items});
 });
 
-router.post("/joke", async (req, res) => {
+router.post("/joke", 
+  isLoggedIn,
+  async (req, res) => {
   const prompt = req.body.prompt;
   try {
     const completion = await openai.createCompletion({
@@ -17,6 +23,15 @@ router.post("/joke", async (req, res) => {
       max_tokens: 1024,
     });
 
+    // save prompt and answer to database
+    const promptItem = new PromptItem({
+      userId: req.user._id,
+      requests: prompt,
+      answer: completion.data.choices[0].text,
+      demo: "joke",
+    });
+    await promptItem.save();
+
     // pass prompt and answer parameters
     res.render(
       "joke/result", 
@@ -25,6 +40,24 @@ router.post("/joke", async (req, res) => {
   } catch(error) {
     console.log(error);
   }
+});
+
+
+router.get("/joke/result/:id",
+  isLoggedIn,
+  async (req, res, next) => {
+    const promptItem = await PromptItem.findOne({_id:req.params.id});
+    res.render("joke/result", {
+      prompt: promptItem.requests,
+      answer: promptItem.answer,
+    });
+});
+
+router.get("/joke/remove/:id",
+  isLoggedIn,
+  async (req, res, next) => {
+    await PromptItem.deleteOne({_id:req.params.id});
+    res.redirect("/joke");
 });
 
 module.exports = router;
